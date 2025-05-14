30bf142c88edca7d94c149161853c455b32d2379
Ankit Lal Sinha <ankitsinha25061909@gmail.com>
2025-05-14 14:03:59 +0530

    folder Structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/dbConfig');
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database
+connectDB();
 
 // Routes
 
@@ -16,5 +18,10 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    }
-    );
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+})
